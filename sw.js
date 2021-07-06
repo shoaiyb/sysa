994c24c30ecc0bd8783aa410047e838cb1aaf1c7
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-07-06 12:30:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
 const staticShoaiybSysa = "shoaiyb-sysa-site-v1";
 const assets = [
   "/",
-  "/../.",
   "/index.html",
   "/404.html",
   "/assets/images/logo.svg"
+  "/assets/images/logo.png",
+  "/assets/images/favicon-16x16.png",
+  "/assets/images/favicon-32x32.png",
+  "/assets/images/apple-touch-icon.png",
+  "/assets/css/prism.css",
+  "/assets/css/theme.css",
+  "/assets/js/theme.js",
+  "/assets/js/lazyload.js",
+  "/assets/js/lunr.js",
+  "/assets/js/lunrsearchengine.js",
+  "/assets/js/prism.min.js",
+  "/assets/js/jquery.min.js"
 ];
 
 self.addEventListener("install", installEvent => {
